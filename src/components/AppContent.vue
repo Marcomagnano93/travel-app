b06ed2e128b0237030b0891956de6a5e85fadd17
Marcomagnano93 <marcog.magnano@gmail.com>
2024-08-18 15:49:07 +0200

    ADD: form

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -21,8 +21,14 @@ export default {
       <div class="container py-5 h-100">
         <div class="row aling-items-center flex-column justify-content-center h-100">
           <div class="col-auto text-center">
-            <h1>Pianifica il tuo viaggio</h1>
+            <h1>Descrivi tuo viaggio</h1>
+            <form action="">
+              <input type="text" placeholder="dove sei stato?" v-model="name">
+              <input type="text" placeholder="quanto sei stato?" v-model="days">
+              <input type="text" placeholder="descrivi la tappa" v-model="description">
+              <input type="text" placeholder="valuta" v-model="rating">
 
+            </form>
             
               <div v-for="(travel, i) in store.travels" :key="i">
                   <h5>{{ travel.name }}</h5>
