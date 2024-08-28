e03f70162b77a1ba8f356175ae0e4e972dd2dcc1
Marcomagnano93 <marcog.magnano@gmail.com>
2024-08-28 17:01:34 +0200

    ADD: footer

--- README.md
+++ README.md
@@ -3,5 +3,3 @@ CODING TRAINING TRAVEL APP
 
 NEXT_STEPS
 - Footer
-- Validazioni
-- FIX toFixed() bug
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -15,8 +15,24 @@ export default {
 
   <footer>
     <section class="bg-dark">
-      <div class="container py-5 text-center text-white">
-        <h1>FOOTER</h1>
+      <div class="container py-3 text-center goldsand">
+        <div class="row">
+          <div class="col">
+            <p>Applicativo scritto in:</p>
+            <ul class="stars">
+              <li>Vue.Js</li>
+              <li>SCSS</li>
+              <li>Bootstrap</li>
+            </ul>
+          </div>
+          <div class="col">
+            <p>I miei contatti: </p>
+            <ul class="stars d-flex gap-4  justify-content-center">
+              <li><div class="btn btn-info btn-sm"><a href="https://www.linkedin.com/in/marcomagnano93/" class="link goldsand ">Linkedin</a></div></li>
+              <li><div class="btn btn-warning btn-sm"><a href="https://github.com/Marcomagnano93" class="link goldsand ">GitHub</a></div></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   </footer>
